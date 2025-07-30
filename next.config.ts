e3072b20f081d66b5983569da796732116e9761f
Reiyo <152931512+RyoK3N@users.noreply.github.com>
2025-07-30 06:37:31 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
-
-import type {NextConfig} from 'next';
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  output: 'standalone',
+  webpack(config) {
+    config.resolve.alias = {
+      ...(config.resolve.alias ?? {}),
+      '@': path.resolve(__dirname, 'src'),
+    };
+    return config;
   },
 };
 
